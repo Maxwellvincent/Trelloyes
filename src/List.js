db6a604eb6da290e40cf878895cb5b2330c2c23e
Louis Maxwell <louismaxwell@knights.ucf.edu>
2020-07-26 23:53:32 -0400

    initial commit

---
+++ src/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card';
+import './List.css'
+
+ class List extends React.Component {
+    render(props){
+        console.log(this.props);
+        // let cards = this.props.cards.map((item,index) => <Card key={index} title={item} />);
+        return(
+            <section className="List">
+                <header>{this.props.header}</header>
+                <div className='List-cards'>
+                    {this.props.cards.map(card => <Card key={card.id} title={card.title} content={card.content}/>)}
+                </div>
+            </section>
+        );
+    }
+}
+
+export default List;
